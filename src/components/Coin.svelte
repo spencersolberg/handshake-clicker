87eedd3258fb0e8e6604ab7e4bfe46a6f05f9a60
Spencer <other.spencersolberg@outlook.com>
2022-03-29 10:55:01 -0400

    Added text effect on coin click

--- src/components/Coin.svelte
+++ src/components/Coin.svelte
@@ -1,15 +1,45 @@
 <script>
   import { save } from "../stores/save.js";
+  import { formatSmall } from "../lib/functions.js";
+  let div;
 
-  const click = () => {
+  const calculateClick = () => {
+    return 1;
+  }
+
+  const click = (e) => {
+    let click = calculateClick();
+
     $save.balance++;
-    $save.stats.hnsAllTime ++;
-    $save.stats.hnsClicked ++;
-    $save.stats.timesClicked ++;
+    $save.stats.hnsAllTime++;
+    $save.stats.hnsClicked++;
+    $save.stats.timesClicked++;
+
+    spawnText(e, click);
+  };
+
+  const spawnText = (e, click) => {
+    let t = document.createElement("p");
+    t.style.left = e.clientX + "px";
+    t.style.top = e.clientY - 20 + "px";
+    t.className = "font-mono text-3xl text-green-400 fixed z-50";
+    t.innerText = "+" + formatSmall(click);
+
+    div.appendChild(t);
+    let opacity = 100;
+    let interval = setInterval(() => {
+      t.style.opacity = String(opacity / 100);
+      t.style.top = (Number(t.style.top.replace("px", "")) - 2) + "px";
+      opacity--;
+      if (opacity == 0) {
+        t.remove();
+        clearInterval(interval);
+      }
+    }, 20);
   };
 </script>
 
-<div class="select-none" on:click={click} on:mousedown>
+<div class="select-none" bind:this={div} on:click={click} on:mousedown>
   <img
     src="hns.svg"
     id="coin"
